c252c8dc6a19e1c32e372529436e4c9a0da9a08d
Joy <joyglory213@gmail.com>
2023-12-07 16:36:21 +0300

    completed

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears(
+    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name text,
+    age integer,
+    sex text,
+    color text,
+    temperament text,
+    alive boolean
+);
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(name,age,sex,color,temperament,alive) 
+VALUES
+  ('Mr. Chocolate', 20, 'M', 'Brown', 'Friendly', 1),
+  ('Rowdy', 3, 'M', 'Black', 'Playful', 1),
+  ('Tabitha', 5, 'F', 'Gray', 'Calm', 1),
+  ('Sergeant Brown', 8, 'M', 'Brown', 'Grumpy', 1),
+  ('Melissa', 6, 'F', 'White', 'Curious', 1),
+  ('Grinch', 4, 'M', 'Green', 'Sly', 1),
+  ('Wendy', 7, 'F', 'Blue', 'Cheerful', 1),
+  (NULL, NULL, NULL, NULL, NULL, NULL);
